ca9a9bbce3b0709cb7dffc4a1f7d31e6af308e32
elvee <lowie@beecave.nl>
2024-09-22 11:19:32 +0200

    feat ✨: Added script to copy custom patterns from current directory to ~/.config/fabric/patterns.

---
+++ copy-custom-patterns.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for item in ./*/; do
+    read -p "Do you want to copy '$item' to ~/.config/fabric/patterns? (y/n) " choice
+    case "$choice" in 
+      y|Y ) cp -R "$item" ~/.config/fabric/patterns;;
+      * ) echo "Skipping '$item'";;
+    esac
+done
+echo "Copied all the files. You can run `fabric -l` to check if they are avaliable to use with fabric."
